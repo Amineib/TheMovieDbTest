61ced2f7771c047bc64c68b7889f1b2533cf59a9
Amine IB <mohamedamine.ib@gmail.com>
2023-06-11 11:49:54 +0200

    refactor build.gradle add buildSrc

---
+++ buildSrc/src/main/java/BuildPlugins.kt
@@ -0,0 +1,5 @@
+object BuildPlugins {
+    const val androidApplication : String  = "com.android.application"
+    const val androidLibrary : String  = "com.android.library"
+    const val kotlinAndroid : String  = "org.jetbrains.kotlin.android"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,32 @@
+object Versions {
+    const val gradlePlugin = "7.3.1"
+    const val kotlin = "1.8.10"
+
+    const val coreKtx = "1.10.1"
+    const val appCompat = "1.6.1"
+    const val material = "1.9.0"
+    const val constraintLayout = "2.1.4"
+    const val lifecycleRuntimeKtx = "2.6.1"
+    const val kTor = "2.2.1"
+
+
+
+    const val jUnit = "4.13.2"
+    const val androidXTestJUnit = "1.1.5"
+    const val espressoCore = "3.5.1"
+
+    const val compose = "1.4.3"
+    const val activityCompose = "1.7.2"
+    const val retrofit = "2.9.0"
+    const val okHttpLoggingInterceptor = "4.2.1"
+    const val jsonSerialization = "1.2.2"
+    const val coil = "1.3.2"
+    const val daggerHilt = "2.44"
+    const val daggerHiltNavigation = "1.0.0"
+    const val daggerHiltNavigationCompose = "1.0.0-alpha03"
+
+    const val paging = "3.1.1"
+    const val pagingCompose = "3.2.0-beta01"
+    const val navigationCompose = "2.5.3"
+
+}
